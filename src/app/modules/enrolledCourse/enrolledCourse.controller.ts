5f060203c372f52dcb8230982c89501f46866453
Md. Abdul Satter <116937243+AbdulSatterism@users.noreply.github.com>
2024-08-20 19:11:07 +0600

    ok

--- src/app/modules/enrolledCourse/enrolledCourse.controller.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.controller.ts
@@ -18,6 +18,23 @@ const createEnrolledCourse = catchAsync(async (req, res) => {
   });
 });
 
+const getAllEnrolledCourses = catchAsync(async (req, res) => {
+  const facultyId = req.user.userId;
+
+  const result = await EnrolledCourseServices.getAllEnrolledCoursesFromDB(
+    facultyId,
+    req.query,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Enrolled courses are retrieved successfully',
+    meta: result.meta,
+    data: result.result,
+  });
+});
+
 const getMyEnrolledCourses = catchAsync(async (req, res) => {
   const studentId = req.user.userId;
 
@@ -54,4 +71,5 @@ export const EnrolledCourseControllers = {
   createEnrolledCourse,
   getMyEnrolledCourses,
   updateEnrolledCourseMarks,
+  getAllEnrolledCourses,
 };
